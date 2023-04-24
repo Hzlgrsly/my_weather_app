f32ead76f9941bcc8689110bc2757b0357124934
Hazel <119948286+Hzlgrsly@users.noreply.github.com>
2023-04-24 10:09:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -87,3 +87,10 @@ function displayWeatherCondition(response) {
     response.data.weather[0].main;
   getForecast(response.data.coord);
 }
+
+function getForecast(coordinates) {
+  let apiKey = "9eca7aac0b071aa16e3cb063adba0785";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+  console.log(apiUrl);
+}
